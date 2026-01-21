274dde678b9cd4d529ad1dbd845ea5ad9982e5ae
pseudo-archaeologist <ks102anc@gmail.com>
2026-01-21 11:16:13 +1100

    Delete wash-communities-map/Shefa directory

--- wash-communities-map/Shefa/data/2019_8.js
+++
@@ -1,1 +0,0 @@
-var json_2019_8 = {"type":"FeatureCollection","name":"2019_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- wash-communities-map/Shefa/data/350500_3.js
+++
@@ -1,1 +0,0 @@
-var json_350500_3 = {"type":"FeatureCollection","name":"350500_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
